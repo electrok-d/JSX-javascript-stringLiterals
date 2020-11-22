88f54f2dfbbb4b0d3bdc8d5ad4509b83fdbb7bd4
Jed Black <62964873+electrok-d@users.noreply.github.com>
2020-11-22 14:36:31 -0800

    Created variables and string literals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Jed";
+const lName = "Black";
+const number = 8;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p> Your lucky number is {number}</p>
+  </div>,
+
+  document.getElementById("root")
+);
